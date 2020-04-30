b438ce5c07374d5cb06f9ceadc8e14e5b2b5a917
Educhin <c.s.arinze@gmail.com>
2020-04-29 20:45:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function findMinAndRemove(array){
-
+    let min = array[0];
+    let j = 0
+    for (let i = 0; i < array.length; i++){
+        if (array[i] < min) {
+            min = array[i]
+            j = i
+        }
+    }
+   return array.splice(j, 1)[0]  
 }
 
 function insertionSort(array){
-
+    let sorted = []
+    while (array.length > 0) {
+        sorted.push(findMinAndRemove(array))
+    }
+    return sorted
 }
