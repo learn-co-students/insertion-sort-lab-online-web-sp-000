6fd978efcfea504d78058000c429c3c7f916430b
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-01 09:28:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
+  let min = array[0];
+  let idx = 0;
+  for(let i=1; i<array.length; i++) {
+    if(array[i] < min) {
+      min = array[i];
+      idx = i;
+    }
+  }
 
+  array.splice(idx, 1);
+  return min;
 }
 
 function insertionSort(array){
+  let returnArray = [];
+  while (array.length !== 0) {
+    const smallest = findMinAndRemove(array);
+    returnArray.push(smallest);
+  }
 
+  return returnArray;
 }
