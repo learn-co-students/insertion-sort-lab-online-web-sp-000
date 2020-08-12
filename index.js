beaee25514baa9776a9103a4c0079b0b660018af
boom100100 <bsd231@nyu.edu>
2020-08-12 19:33:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function findMinAndRemove(array){
-
+  const reducer = (a, b) => a < b ? a : b;
+  return array.splice(array.indexOf(array.reduce(reducer)), 1)[0];
 }
 
 function selectionSort(array){
+  let target = [];
 
+  for (let i = array.length - 1; i >= 0; i--){
+    target.push(findMinAndRemove(array));
+  }
+
+  return target;
 }
