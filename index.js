f8c4a31c8e42105a23c0280906ed98a1ed9fc12b
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-11-26 06:20:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function findMinAndRemove(array){
-
+  var min = array[0]
+  var minIndex = 0
+  for (var i = 1; i < array.length; i++)
+  {
+    if (min > array[i])
+    {
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1)
+  return min
 }
 
 function selectionSort(array){
-
+  
+  let sorted = []
+  let newMin
+  
+  while (array.length != 0)
+  {
+    sorted.push(findMinAndRemove(array))
+  }
+  
+  return sorted
 }
