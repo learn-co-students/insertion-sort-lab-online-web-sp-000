6a7dd85d0370e15a8aca73406a9f2ea989c03f33
wkdewey <wkdewey@gmail.com>
2020-12-01 13:58:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  let min = array[0];
+  let minIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
 }
 
-function selectionSort(array){
-
+function selectionSort(array) {
+  let sortedArray = [];
+  let originalArray = [...array];
+  for (let i = 0; i < array.length; i++) {
+    sortedArray[i] = findMinAndRemove(originalArray);
+  }
+  return sortedArray;
 }
