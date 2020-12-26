ffafe4645e70b8410054b2ad04581b74ee692981
Amanda Rolon <abrolon87@gmail.com>
2020-12-25 23:29:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function findMinAndRemove(array){
-
+  let sorted = array.sort()
+  return sorted.shift()
 }
 
 function selectionSort(array){
-
+  let newMin;
+  let newArr = [];
+  while(array.length > 0) {
+    newMin = findMinAndRemove(array)
+    newArr.push(newMin)
+  }
+  return newArr
 }
