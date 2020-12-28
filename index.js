ab9586a6d8032ffdf7e6bdebbf2a7e8f62842431
jessicagomesng <jgnpeluso@gmail.com>
2020-12-28 15:21:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 function findMinAndRemove(array){
+    let min = array[0];
+    let minIndex = 0;
 
+    for (let i = 0; i < array.length; i++ ) {
+        if (array[i] < min) {
+            min = array[i];
+            minIndex = i; 
+        }
+    }
+
+    array.splice(minIndex, 1)
+    return min;
 }
 
 function selectionSort(array){
+    let n;
+    let sorted = [];
 
+    while (array.length != 0 ) {
+        n = findMinAndRemove(array);
+        sorted.push(n);
+    }
+
+    return sorted;
 }
