0e2782779464631c31ec4045cf47f4d30bc37c66
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-18 17:58:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function findMinAndRemove(array){
+  let min = array[0];
+  let minIdx = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIdx = i;
+    }
+  }
 
+  array.splice(minIdx, 1);
+  return min;
 }
 
 function selectionSort(array){
+  let newMin;
+  let sorted = [];
+  while (array.length != 0) {
+    newMin = findMinAndRemove(array);
+    sorted.push(newMin);
+  }
 
+  return sorted;
 }
