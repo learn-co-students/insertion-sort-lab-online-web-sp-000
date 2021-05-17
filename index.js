8cc81db6e8214a7c338b8e35cdac39c4dd4d2342
deliaconstantino <dconstan92@gmail.com>
2021-05-17 11:44:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
+  let min = array[0];
+  let index;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      index = i;
+    }
+  }
 
+  array.splice(index, 1);
+  return min;
 }
 
 function selectionSort(array){
+  let sorted = []
 
+  while (array.length > 0) {
+    sorted.push(findMinAndRemove(array));
+  }
+
+  return sorted;
 }
