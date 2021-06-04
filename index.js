5e8e4411b08983796bee43364c254421759c49ae
soncrayon <arnold.curtis@gmail.com>
2021-06-04 11:44:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+    let min = array[0]
+    let minIndex = 0
+    for(let item of array) {
+        if(item < min){
+            min = item
+            minIndex = array.indexOf(item)
+        }
+    }
+    array.splice(minIndex, 1)
+    return min
 }
 
 function selectionSort(array){
-
+    let sortedArray = []
+    let minValue
+    while (array.length != 0) {
+        minValue = findMinAndRemove(array)
+        sortedArray.push(minValue)
+    }
+    return sortedArray
 }
